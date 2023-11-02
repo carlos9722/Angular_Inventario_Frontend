276398743ba1fd2bf6b0e767361abcf7ede9cac9
cpena <cpena@mangus.org>
2023-11-02 17:34:05 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductoListaComponent } from './producto-lista/producto-lista.component';
+import { AgregarProductoComponent } from './agregar-producto/agregar-producto.component';
+import { EditarProductoComponent } from './editar-producto/editar-producto.component';
 
-const routes: Routes = [];
+//http:localhost:4200/productos
+const routes: Routes = [
+  {path:'productos', component: ProductoListaComponent},
+  {path:'agregar-producto', component: AgregarProductoComponent},
+  {path:'editar-producto/:id', component: EditarProductoComponent},
+  {path:'', redirectTo: 'productos', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
